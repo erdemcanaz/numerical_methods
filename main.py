727516a827b35635d63b78ebccac7d15bfdb8ebd
Erdem <erdemcanaz@gmail.com>
2024-01-16 18:46:28 +0300

    nonlinear solvers - false position method

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import nonlinear_solvers
 def root_2(x):
     return x**2 - 2
 
-r = nonlinear_solvers.bisection_method(f=root_2, ABSOLUTE_Y_ERROR= 1e-15, left_x=0, right_x=2, verbose=False)
-print(r)
+r = nonlinear_solvers.bisection_method(f=root_2, ABSOLUTE_Y_ERROR= 1e-6, left_x=0, right_x=2, verbose=True)
+r = nonlinear_solvers.false_position_method(f=root_2, ABSOLUTE_Y_ERROR= 1e-6, left_x=0, right_x=2, verbose=True)
--- nonlinear_solvers.py
+++ nonlinear_solvers.py
@@ -1,3 +1,54 @@
+def false_position_method(f = None, left_x = None, right_x = None, ABSOLUTE_Y_ERROR = 1e-15,  ABSOLUTE_X_ERROR = 1e-15, RELATIVE_X_ERROR =  1e-15, MAX_ITERATION =  55, verbose = True):
+    #Absolute y error: if f(x) is smaller than this value, then return x
+    #Absolute x error: if approximated x is closer to exact value smaller than this value, then return x
+    #Relative x error: if (bracket length)/(x_left) smaller than this value, then return x
+    #Max iteration: if iteration count is larger than this value, then return x. Note that 2^50 > 1.1258999e+15.   
+                      
+    if f == None:
+      raise ValueError("f is not defined.")
+    elif left_x == None or right_x == None:
+      raise ValueError("Initial brackets are not given.")
+    elif left_x >= right_x:
+        raise ValueError("Left bracket is not smaller than right bracket.")
+    elif f(x=left_x) == 0:
+      return left_x
+    elif f(x=right_x) == 0:
+        return right_x
+    elif f(x=left_x) * f(x=right_x) > 0:
+        raise ValueError("Initial brackets do not guaranteed to have a solution by IVT.")    
+    
+    if(verbose):print(f"\nApplying false-position method to f(x)={f.__name__} with initial brackets ({left_x},{right_x})")
+    iteration_counter = 0
+    while True:        
+
+        #update brackets
+        m = (f(x=right_x) - f(x=left_x))/(right_x - left_x)
+        del_x = -f(x=left_x)/m
+        x_next = left_x + del_x
+        y_next = f(x=x_next)
+
+
+        if(abs(y_next) < ABSOLUTE_Y_ERROR):#Absolute y error:
+            if(verbose):print(f"Absolute y error: {abs(y_next)}")
+            return x_next
+        elif f(x=left_x) * f(x=x_next) < 0:
+            right_x = x_next
+        else:
+            left_x = x_next
+
+        #check convergence
+        if (right_x - left_x < ABSOLUTE_X_ERROR): #Absolute x error:
+            if(verbose):print(f"Absolute x error: {right_x - left_x}")
+            return x_next
+        elif( (right_x - left_x)/(min(abs(left_x),abs(right_x))+1e-10) < RELATIVE_X_ERROR ): #Relative x error
+            if(verbose):print(f"Relative x error: {(right_x - left_x)/(min(abs(left_x),abs(right_x)))}")
+            return x_next
+        elif iteration_counter >= (MAX_ITERATION-1): #Max iteration
+            if(verbose):print(f"Max iteration: {iteration_counter+1}")
+            return x_next
+        
+        if(verbose):print(f"Iteration: {iteration_counter+1} , ({left_x} , {right_x}), y={f(x=(left_x + right_x)/2)}" )
+        iteration_counter += 1
 
 def bisection_method(f = None, left_x = None, right_x = None, ABSOLUTE_Y_ERROR = 1e-15,  ABSOLUTE_X_ERROR = 1e-15, RELATIVE_X_ERROR =  1e-15, MAX_ITERATION =  55, verbose = True):
     #Absolute y error: if f(x) is smaller than this value, then return x
@@ -17,12 +68,10 @@ def bisection_method(f = None, left_x = None, right_x = None, ABSOLUTE_Y_ERROR =
         return right_x
     elif f(x=left_x) * f(x=right_x) > 0:
         raise ValueError("Initial brackets do not guaranteed to have a solution by IVT.")    
- 
+    
+    if(verbose):print(f"\nApplying bisection method to f(x)={f.__name__} with initial brackets ({left_x},{right_x})")
     iteration_counter = 0
-    x_estimate = None
     while iteration_counter < MAX_ITERATION:
-
-        if(verbose):print(f"Iteration: {iteration_counter+1} , ({left_x},{right_x}), y={f(x=(left_x + right_x)/2)}" )
 
         #update brackets
         x_mid = (left_x + right_x)/2
@@ -39,15 +88,16 @@ def bisection_method(f = None, left_x = None, right_x = None, ABSOLUTE_Y_ERROR =
         if (right_x - left_x < ABSOLUTE_X_ERROR): #Absolute x error: 
             if(verbose):print(f"Absolute x error: {right_x - left_x}")
             return x_mid
-        elif( (right_x - left_x)/(min(abs(left_x),abs(right_x))) < RELATIVE_X_ERROR ): #Relative x error
+        elif( (right_x - left_x)/(min(abs(left_x),abs(right_x))) < RELATIVE_X_ERROR+1e-10): #Relative x error
             if(verbose):print(f"Relative x error: {(right_x - left_x)/(min(abs(left_x),abs(right_x)))}")
             return x_mid
         elif iteration_counter >= (MAX_ITERATION-1): #Max iteration
             if(verbose):print(f"Max iteration: {iteration_counter+1}")
             return x_mid        
 
+        if(verbose):print(f"Iteration: {iteration_counter+1} , ({left_x} , {right_x}), f={f(x=(left_x + right_x)/2)}" )
         iteration_counter += 1
-    return x_estimate
+
         
         
 
